691950d2c556cd62f32a4a40c336b24fa121cf51
Toni Patsias <enchanted.toilet@gmail.com>
2023-03-23 11:35:26 +0000

    removed Hello World text

--- backend/index.js
+++ backend/index.js
@@ -39,10 +39,6 @@ let persons = [
       }
 ]
 
-app.get("/", (request, response) => {
-    response.send("<h1>Hello World!</h1>")
-})
-
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(p =>  p.id === id)
